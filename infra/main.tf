b8478e50974ca74c3e1e88de0faee372aa36d583
Louis-Philippe Bossé <louis-philippe.bosse@sunnyexperience.ca>
2025-05-04 08:27:26 -0400

    Refactor Terraform configuration by removing redundant provider block and ensuring proper repository setup

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_artifact_registry_repository" "product_matching_app" {
   name         = "product-matching-app"
   repository_id = "product-matching-app"
