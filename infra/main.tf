af6f7251b0b6e45cd48c185276327cfb272fabfd
Louis-Philippe Bossé <louis-philippe.bosse@sunnyexperience.ca>
2025-05-04 08:39:57 -0400

    Uncomment and push Google Cloud Run service configuration

--- infra/main.tf
+++ infra/main.tf
@@ -4,42 +4,43 @@ resource "google_artifact_registry_repository" "product_matching_app" {
   location     = "northamerica-northeast1"
   description  = "Artifact Registry repository for the Product Matching app"
 }
-# resource "google_cloud_run_service" "flask_app" {
-#   name     = "product-matching-app"
-#   location = var.region
 
-#   template {
-#     spec {
-#       containers {
-#         image = "gcr.io/${var.project_id}/product-matching-app:latest"
-#         ports {
-#           container_port = 5000
-#         }
-#         resources {
-#           limits = {
-#             memory = "512Mi"
-#             cpu    = "1"
-#           }
-#         }
-#       }
-#     }
-#   }
+resource "google_cloud_run_service" "flask_app" {
+  name     = "product-matching-app"
+  location = var.region
 
-#   traffic {
-#     percent         = 100
-#     latest_revision = true
-#   }
-# }
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.project_id}/product-matching-app:latest"
+        ports {
+          container_port = 5000
+        }
+        resources {
+          limits = {
+            memory = "512Mi"
+            cpu    = "1"
+          }
+        }
+      }
+    }
+  }
 
-# resource "google_cloud_run_service_iam_policy" "noauth" {
-#   location    = google_cloud_run_service.flask_app.location
-#   service     = google_cloud_run_service.flask_app.name
-#   policy_data = data.google_iam_policy.noauth.policy_data
-# }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
 
-# data "google_iam_policy" "noauth" {
-#   binding {
-#     role    = "roles/run.invoker"
-#     members = ["allUsers"]
-#   }
-# }
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.flask_app.location
+  service     = google_cloud_run_service.flask_app.name
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
